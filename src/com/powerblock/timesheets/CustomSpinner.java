9d8f7d5eea9aca4dc8f5de3b4aaf3b1cc99e2519
Eoin Davey <eoin.davey@yahoo.com>
2014-07-02 12:49:46 +0100

    Added files from alpha

---
+++ src/com/powerblock/timesheets/CustomSpinner.java
@@ -0,0 +1,67 @@
+package com.powerblock.timesheets;
+
+import java.util.Scanner;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class CustomSpinner extends Spinner {
+	private XmlHandler mXmlHandler;
+	private String[] items;
+
+	public CustomSpinner(Context context) {
+		super(context);
+	}
+	
+	public CustomSpinner(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init(attrs);
+	}
+
+	public CustomSpinner(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init(attrs);
+	}
+	
+	private void init(AttributeSet attrs){
+		mXmlHandler = MainActivity.getXmlHandler();
+		TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.CustomSpinner);
+		CharSequence entry = a.getText(R.styleable.CustomSpinner_entries);
+		Log.v("Test","Entry = "+entry);
+		a.recycle();
+		items = mXmlHandler.getStrings(entry.toString());
+		if(entry != null && items != null){
+			ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, items);
+			super.setAdapter(adapter);
+		}
+		
+	}
+	public void select(String s){
+		int position = 0;
+		for(int i = 0; i < items.length; i ++){
+			if(items[i].equalsIgnoreCase(s)){
+				position = i;
+				break;
+			}
+		}
+		setSelection(position);
+	}
+	public String getString(){
+		String s = (String) getSelectedItem();
+		Scanner sc = new Scanner(s);
+		if(sc.next().equalsIgnoreCase("Choose")){
+			sc.close();
+			sc = null;
+			return "None Selected";
+		} else{
+			sc.close();
+			sc = null;
+			return s;
+		}
+	}
+
+}
---
+++ src/com/powerblock/timesheets/MultiChoiceSpinner.java
@@ -0,0 +1,153 @@
+package com.powerblock.timesheets;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.DialogInterface.OnMultiChoiceClickListener;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MultiChoiceSpinner extends Spinner implements
+		OnMultiChoiceClickListener, OnCancelListener {
+	private CharSequence[] items;
+	private boolean[] selected;
+	private String defaultText = "Choose Items";
+	private MultiSpinnerListener listener;
+	private static XmlHandler mXmlHandler;
+	
+	public MultiChoiceSpinner(Context context) {
+		super(context);
+	}
+	
+	public MultiChoiceSpinner(Context context, AttributeSet arg1){
+		super(context,arg1);
+		init(arg1);
+	}
+	
+	public MultiChoiceSpinner(Context context, AttributeSet arg1, int arg2){
+		super(context,arg1, arg2);
+		init(arg1);
+	}
+	
+	public CharSequence[] getSelectedItems(){
+		int total = 0;
+		for(int i = 0; i < selected.length; i ++){
+			if(selected[i] == true){
+				total += 1;
+			}
+		}
+		int current = 0;
+		CharSequence[] selectedItems = new CharSequence[total];
+		for(int i = 0; i < selected.length; i++){
+			if(selected[i] == true){
+				selectedItems[current] = items[i];
+				current += 1;
+			}
+		}
+		
+		return selectedItems;
+	}
+	
+	private void init(AttributeSet attrs){
+		mXmlHandler = MainActivity.getXmlHandler();
+		if(mXmlHandler == null){
+			Log.v("Test", "Xml handler is null");
+			return;
+		}
+		TypedArray a = getContext().obtainStyledAttributes(attrs,R.styleable.MultiSpinner);
+		CharSequence entry = a.getText(R.styleable.MultiSpinner_multi_entries);
+		a.recycle();
+		CharSequence[] entries = mXmlHandler.getStrings(entry.toString());
+		if(entries != null){
+			selected = new boolean[entries.length];
+			items = entries;
+			ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_spinner_item, new String[]{defaultText});
+			setAdapter(adapter);
+		}
+		listener = new MultiSpinnerListener() {
+			
+			@Override
+			public void onItemsSelected(CharSequence[] selected) {
+				
+			}
+		};
+	}
+
+	@Override
+	public void onCancel(DialogInterface dialog) {
+		StringBuffer spinnerBuffer = new StringBuffer();
+		boolean someSelected = false;
+		String spinnerText = defaultText;
+		if(items != null){
+			for(int i = 0; i < items.length; i ++){
+				if(selected[i] == true){
+					spinnerBuffer.append(items[i]);
+					spinnerBuffer.append(", ");
+					someSelected = true;
+				}
+			}
+			if (someSelected){
+				spinnerText = spinnerBuffer.toString();
+				if(spinnerText.length() > 2){
+					spinnerText = spinnerText.substring(0, spinnerText.length() - 2);
+				}
+			} else {
+				spinnerText = defaultText;
+			}
+		}
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_spinner_item, new String[] {spinnerText});
+		setAdapter(adapter);
+		listener.onItemsSelected(getSelectedItems());
+	}
+	
+	@Override
+	public boolean performClick(){
+		AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+		builder.setMultiChoiceItems(items,selected, this);
+		builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.cancel();
+			}
+		});
+		builder.setOnCancelListener(this);
+		builder.show();
+		return true;
+	}
+
+	@Override
+	public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+		if(isChecked)
+			selected[which] = true;
+		else
+			selected[which] = false;
+
+	}
+	
+	public void setItems(CharSequence[] items, String allText, MultiSpinnerListener listener){
+		this.items = items;
+		this.defaultText = allText;
+		this.listener = listener;
+		
+		selected = new boolean[items.length];
+		for(int i = 0; i <selected.length; i ++){
+			selected[i] = false;
+		}
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_spinner_item, new String[]{allText});
+		setAdapter(adapter);
+	}
+	
+	public void setListener(MultiSpinnerListener listener){
+		this.listener = listener;
+	}
+	
+	public interface MultiSpinnerListener{
+		public void onItemsSelected(CharSequence[] selected);
+	}
+
+}
