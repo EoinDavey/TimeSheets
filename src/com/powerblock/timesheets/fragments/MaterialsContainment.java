6a8f888de02cf0c7f0f300e26f371a830b513d47
Eoin Davey <eoin.davey@yahoo.com>
2014-07-08 10:56:31 +0100

    Create MaterialsContainment.java

---
+++ src/com/powerblock/timesheets/fragments/MaterialsContainment.java
@@ -0,0 +1,47 @@
+package com.powerblock.timesheets.fragments;
+
+import com.powerblock.timesheets.ExcelHandler;
+import com.powerblock.timesheets.MainActivity;
+import com.powerblock.timesheets.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MaterialsContainment extends Fragment {
+	private ExcelHandler mExcelHandler;
+	private View mView;
+
+	public MaterialsContainment(){
+		mExcelHandler = MainActivity.getExcelHandler();
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		setHasOptionsMenu(true);
+		mView = mExcelHandler.read(inflater, container, R.layout.materials_containment, ExcelHandler.EXCEL_SECTION_MATERIALS_CONTAINMENT);
+		if(mView == null)
+			mView = inflater.inflate(R.layout.materials_containment, container,false);
+		return mView;
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+		inflater.inflate(R.menu.save_menu, menu);
+		super.onCreateOptionsMenu(menu, inflater);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		if(item.getItemId() == R.id.action_save){
+			mExcelHandler.write(ExcelHandler.EXCEL_SECTION_MATERIALS_CONTAINMENT,mView);
+			return true;
+		}
+		return false;
+	}
+}
